90ad065ae9366ae1db75ce102447df8e93bb2f25
Mathias Markl <mathias@mukprojects.at>
2017-10-04 22:24:06 +0200

    update

--- dms/models/dms_lock.py
+++ dms/models/dms_lock.py
@@ -19,4 +19,40 @@
 #
 ###################################################################################
 
-from . import main
+import logging
+
+from odoo import models, api, fields
+
+_logger = logging.getLogger(__name__)
+
+class Lock(models.Model):
+    _name = 'muk_dms.lock'
+    _description = "Directory or File Lock"
+
+    name = fields.Char(
+        compute='_compute_name',
+        string="Name")
+
+    locked_by = fields.Char(
+        string="Locked by",
+        required=True)
+    
+    locked_by_ref = fields.Reference(
+        [('res.users', 'User')],
+        string="User Reference")
+
+    lock_ref = fields.Reference(
+        [],
+        string="Object Reference",
+        required=True)
+    
+    token = fields.Char(
+        string="Token")
+    
+    operation = fields.Char(
+        string="Operation")
+    
+    @api.one
+    @api.depends('lock_ref')
+    def _compute_name(self):
+        self.name = "Lock for " + str(self.lock_ref.name)
