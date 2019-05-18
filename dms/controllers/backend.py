46cb6873fed6b3c579a6e6a163cdd694bec6d9bc
MuK IT GmbH <mathias.markl@mukit.at>
2019-05-18 16:09:07 +0000

    publish muk_dms - 12.0

--- dms/controllers/backend.py
+++ dms/controllers/backend.py
@@ -17,5 +17,18 @@
 #
 ###################################################################################
 
-from . import backend
-from . import onboarding
+import logging
+
+from odoo import _, http
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+class BackendController(http.Controller):
+
+    @http.route('/config/muk_dms.forbidden_extensions', type='json', auth="user")
+    def forbidden_extensions(self, **kw):
+        params = request.env['ir.config_parameter'].sudo()
+        return {
+            'forbidden_extensions': params.get_param("muk_dms.forbidden_extensions", default="")
+        }
